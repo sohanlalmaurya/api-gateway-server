6af63c3f64aea23ad61570affc2354bdbe9ab83f
Sohan Maurya <sohmaury@publicisgroupe.net>
2019-12-10 15:18:18 +0530

    first commit

---
+++ src/test/java/com/sapient/api/gateway/server/ApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapient.api.gateway.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
