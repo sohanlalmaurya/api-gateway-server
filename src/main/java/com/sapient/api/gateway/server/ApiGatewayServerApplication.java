6f7d29ec2406c279da72134136fa4ca2a00ebed2
sohan.maurya <sohanlal.maurya@gmail.com>
2020-04-26 00:47:59 +0530

    implement sleuth

--- src/main/java/com/sapient/api/gateway/server/ApiGatewayServerApplication.java
+++ src/main/java/com/sapient/api/gateway/server/ApiGatewayServerApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.netflix.eureka.registry.rule.AlwaysMatchInstanceStatusRule;
+
+import brave.sampler.Sampler;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
@@ -14,4 +19,9 @@ public class ApiGatewayServerApplication {
 		SpringApplication.run(ApiGatewayServerApplication.class, args);
 	}
 
+	@Bean
+	public Sampler defaultSampler() {
+		
+		return Sampler.ALWAYS_SAMPLE;
+	}
 }
